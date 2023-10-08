c9f39447614bca84c55a3ce69f21471bf825f326
martinsndifon <martinsndifon@gmail.com>
2023-10-08 23:58:11 +0100

    Initial files for profile card challenge

---
+++ 03-profile-card/src/index.js
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
