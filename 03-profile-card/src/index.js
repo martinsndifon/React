ae51541b7f3f8ef9f3116865dac993e428bfa390
martinsndifon <martinsndifon@gmail.com>
2023-10-09 10:36:08 +0100

    completed the profile card challenge

--- 03-profile-card/src/index.js
+++ 03-profile-card/src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles.css';
+import './index.css';
 
 function App() {
   return (
@@ -10,6 +10,55 @@ function App() {
         <Intro />
         <SkillList />
       </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="profile_image.jpg"
+      alt="profile card pic"
+    ></img>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Martins Ndifon</h1>
+      <p>
+        Full-stack software engineer, passionate about creating and maintaining
+        robust software. Other than coding, I have a pretty interesting lif,
+        playing football, chess, swimming and hanging out with friends and
+        family.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML+CSS" emoji="💪" backgroundColor="blue" />
+      <Skill skill="javascript" emoji="👌" backgroundColor="yellow" />
+      <Skill skill="python" emoji="❤️" backgroundColor="purple" />
+      <Skill skill="django/flask" emoji="😊" backgroundColor="red" />
+      <Skill skill="React" emoji="🤞" backgroundColor="green" />
+      <Skill skill="Git and Github" emoji="👍" backgroundColor="orange" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div
+      className="skill"
+      style={{ backgroundColor: `${props.backgroundColor}` }}
+    >
+      <p>{props.skill}</p>
+      <span>{props.emoji}</span>
     </div>
   );
 }
