2e5e3c7d6d700c8d9ef8d1a36de8b8d9145bb937
martinsndifon <martinsndifon@gmail.com>
2023-10-13 09:21:29 +0100

    complete version of the tip calculator project

--- C06-tip-calculator/src/App.js
+++ C06-tip-calculator/src/App.js
@@ -2,28 +2,34 @@ import { useState } from 'react';
 import './index.css';
 
 function App() {
-  const [bill, setBill] = useState(0);
+  const [bill, setBill] = useState('');
   const [percent, setPercent] = useState(0);
   const [fpercent, setfPercent] = useState(0);
 
   const avg = Math.round((percent + fpercent) / 2);
   const tip = Math.round(bill * (avg / 100));
 
+  function handleReset() {
+    setBill(0);
+    setPercent(0);
+    setfPercent(0);
+  }
+
   return (
     <div>
       <Bill bill={bill} setBill={setBill} />
-      <Service id={1} percent={percent} setPercent={setPercent}>
+      <Service percent={percent} setPercent={setPercent}>
         How did you like the Service?
       </Service>
-      <Service id={2} fpercent={fpercent} setfPercent={setfPercent}>
+      <Service percent={fpercent} setPercent={setfPercent}>
         How did your friend like the Service?
       </Service>
-      <Total bill={bill} tip={tip} />
-      <Reset
-        setBill={setBill}
-        setPercent={setPercent}
-        setfPercent={setfPercent}
-      />
+      {!bill ? null : (
+        <>
+          <Total bill={bill} tip={tip} />
+          <Reset onReset={handleReset} />
+        </>
+      )}
     </div>
   );
 }
@@ -33,7 +39,8 @@ function Bill({ bill, setBill }) {
     <div>
       <span>How much was the bill?</span>{' '}
       <input
-        type="number"
+        type="text"
+        placeholder="Enter bill"
         value={bill}
         onChange={(e) => setBill(Number(e.target.value))}
       />
@@ -41,22 +48,15 @@ function Bill({ bill, setBill }) {
   );
 }
 
-function Service({ id, percent, setPercent, fpercent, setfPercent, children }) {
+function Service({ percent, setPercent, children }) {
   function handleChange(e) {
-    if (id === 1) {
-      setPercent(Number(e.target.value));
-    } else {
-      setfPercent(Number(e.target.value));
-    }
+    setPercent(Number(e.target.value));
   }
 
   return (
     <div>
       <span>{children}</span>
-      <select
-        value={id === 1 ? percent : fpercent}
-        onChange={(e) => handleChange(e)}
-      >
+      <select value={percent} onChange={(e) => handleChange(e)}>
         <option value={0}>Dissatisfied (0%)</option>
         <option value={5}>It was okay (5%)</option>
         <option value={10}>It was good (10%)</option>
@@ -74,14 +74,8 @@ function Total({ bill, tip }) {
   );
 }
 
-function Reset({ setBill, setPercent, setfPercent }) {
-  function handleClick() {
-    setBill(0);
-    setPercent(0);
-    setfPercent(0);
-  }
-
-  return <button onClick={handleClick}>Reset</button>;
+function Reset({ onReset }) {
+  return <button onClick={onReset}>Reset</button>;
 }
 
 export default App;
