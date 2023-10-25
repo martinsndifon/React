590a3cc04ac755ee123882cf8edadaa5d272fc46
martinsndifon <martinsndifon@gmail.com>
2023-10-25 18:14:18 +0100

    Completed the currency converter challenge

--- C08-currency-converter/src/App.js
+++ C08-currency-converter/src/App.js
@@ -1,22 +1,71 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [from, setFrom] = useState('EUR');
+  const [to, setTo] = useState('USD');
+  const [output, setOutput] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleFromChange(e) {
+    setFrom(e.target.value);
+  }
+
+  function handleToChange(e) {
+    setTo(e.target.value);
+  }
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setIsLoading(true);
+    fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`,
+      { signal: controller.signal }
+    )
+      .then((res) => res.json())
+      .then((data) => setOutput(data.rates?.[to] || amount))
+      .then(() => setIsLoading(false))
+      .catch((err) => {
+        if (err.name === 'AbortError') {
+          return;
+        }
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, [amount, from, to]);
+
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <select
+        value={from}
+        onChange={(e) => handleFromChange(e)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select
+        value={to}
+        onChange={(e) => handleToChange(e)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>{output > 0 ? `${output} ${to}` : 'OUTPUT'}</p>
     </div>
   );
 }
