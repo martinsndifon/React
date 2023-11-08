4814da7a9dc126e155ed84e4b38e0a9369093fc8
martinsndifon <martinsndifon@gmail.com>
2023-11-08 15:48:19 +0100

    Global styles with styled components

--- 15-the-wild-oasis/src/App.jsx
+++ 15-the-wild-oasis/src/App.jsx
@@ -1,27 +1,12 @@
 import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-  margin: 1rem;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
 `;
 
 const StyledApp = styled.div`
@@ -31,11 +16,14 @@ const StyledApp = styled.div`
 
 function App() {
   return (
-    <StyledApp>
-      <H1>Hello World</H1>
-      <Button>Check in</Button>
-      <Input type='number' placeholder='Number of guests' />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello World</H1>
+        <Button>Check in</Button>
+        <Input type='number' placeholder='Number of guests' />
+      </StyledApp>
+    </>
   );
 }
 
