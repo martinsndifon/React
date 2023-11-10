6a3426e76a91d3d2166a303ed0b29c061f4f129b
martinsndifon <martinsndifon@gmail.com>
2023-11-10 16:20:42 +0100

    Compound components example

---
+++ 17-compound-components/src/App.js
@@ -0,0 +1,25 @@
+import Counter from "./Counter";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Compound Component Pattern</h1>
+      {/* <Counter
+        iconIncrease="+"
+        iconDecrease="-"
+        label="My NOT so flexible counter"
+        hideLabel={false}
+        hideIncrease={false}
+        hideDecrease={false}
+      /> */}
+
+      <Counter>
+        <Counter.Decrease icon='-' />
+        <Counter.Result />
+        <Counter.Increase icon='+' />
+        <Counter.Label>My super flexible label</Counter.Label>
+      </Counter>
+    </div>
+  );
+}
