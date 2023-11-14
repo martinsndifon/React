85042c7f67942e0f1d8dde4304fc68fd4d9d5ab8
martinsndifon <martinsndifon@gmail.com>
2023-11-14 22:37:51 +0100

    user sign up

--- 15-the-wild-oasis/src/features/authentication/SignupForm.jsx
+++ 15-the-wild-oasis/src/features/authentication/SignupForm.jsx
@@ -3,21 +3,24 @@ import Button from '../../ui/Button';
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
+import { useSignup } from './useSignup';
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
   const { errors } = formState;
+  const { signup, isLoading } = useSignup();
 
-  function onSubmit(data) {
-    console.log(data);
+  function onSubmit({ fullName, email, password }) {
+    signup({ fullName, email, password }, { onSettled: reset });
   }
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow label='Full name' error={errors?.fullName?.message}>
         <Input
+          disabled={isLoading}
           type='text'
           id='fullName'
           {...register('fullName', { required: 'This field is required' })}
@@ -26,6 +29,7 @@ function SignupForm() {
 
       <FormRow label='Email address' error={errors?.email?.message}>
         <Input
+          disabled={isLoading}
           type='email'
           id='email'
           {...register('email', {
@@ -43,6 +47,7 @@ function SignupForm() {
         error={errors?.password?.message}
       >
         <Input
+          disabled={isLoading}
           type='password'
           id='password'
           {...register('password', {
@@ -57,6 +62,7 @@ function SignupForm() {
 
       <FormRow label='Repeat password' error={errors?.passwordConfirm?.message}>
         <Input
+          disabled={isLoading}
           type='password'
           id='passwordConfirm'
           {...register('passwordConfirm', {
@@ -69,10 +75,10 @@ function SignupForm() {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button $variation='secondary' type='reset'>
+        <Button $variation='secondary' type='reset' disabled={isLoading}>
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isLoading}>Create new user</Button>
       </FormRow>
     </Form>
   );
