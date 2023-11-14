0d9bd37ec2e8a536835e7e561e2e39cb1e6b4eb2
martinsndifon <martinsndifon@gmail.com>
2023-11-14 18:42:01 +0100

    Authorization, protecting routes

--- 15-the-wild-oasis/src/features/authentication/useLogin.js
+++ 15-the-wild-oasis/src/features/authentication/useLogin.js
@@ -1,14 +1,16 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { login as loginApi } from '../../services/apiAuth';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
 function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { isLoading, mutate: login } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: () => {
+    onSuccess: (user) => {
+      queryClient.setQueriesData(['user'], user);
       navigate('/dashboard');
     },
     onError: (err) => {
