027c4a0cd80bfcabfd165d9e4dd88114fe03490e
martinsndifon <martinsndifon@gmail.com>
2023-11-14 14:27:36 +0100

    Authentication - User login with supabase

---
+++ 15-the-wild-oasis/src/features/authentication/useLogin.js
@@ -0,0 +1,22 @@
+import { useMutation } from '@tanstack/react-query';
+import { login as loginApi } from '../../services/apiAuth';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+
+function useLogin() {
+  const navigate = useNavigate();
+
+  const { isLoading, mutate: login } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: () => {
+      navigate('/dashboard');
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  return { login, isLoading };
+}
+
+export default useLogin;
