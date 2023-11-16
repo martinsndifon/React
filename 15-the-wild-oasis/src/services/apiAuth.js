0ac72a82971744c97deab7e46e9fa16ee5450c4c
martinsndifon <martinsndifon@gmail.com>
2023-11-16 07:53:10 +0100

    update account details

--- 15-the-wild-oasis/src/services/apiAuth.js
+++ 15-the-wild-oasis/src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
 export async function singup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
@@ -41,4 +41,38 @@ export async function getCurrentUser() {
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  // 1. Update the password or fullName
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) throw new Error(`updateUserError: ${error.message}`);
+
+  if (!avatar) return data;
+
+  // 2. Upload the avatar image
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage
+    .from('avatars')
+    .upload(fileName, avatar);
+
+  if (storageError) throw new Error(`StorageError: ${storageError.message}`);
+
+  // 3. Update avatar in the user
+  const { data: updatedUser, error: updateError } = supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+
+  if (updateError)
+    throw new Error(`UpdateStorageError: ${updateError.message}`);
+
+  return updatedUser;
 }
