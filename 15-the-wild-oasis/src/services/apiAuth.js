99a60ae2559db678e09eb2c3c29dad7cee5622d0
martinsndifon <martinsndifon@gmail.com>
2023-11-16 08:12:56 +0100

    Bug fix

--- 15-the-wild-oasis/src/services/apiAuth.js
+++ 15-the-wild-oasis/src/services/apiAuth.js
@@ -65,11 +65,12 @@ export async function updateCurrentUser({ password, fullName, avatar }) {
   if (storageError) throw new Error(`StorageError: ${storageError.message}`);
 
   // 3. Update avatar in the user
-  const { data: updatedUser, error: updateError } = supabase.auth.updateUser({
-    data: {
-      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
-    },
-  });
+  const { data: updatedUser, error: updateError } =
+    await supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      },
+    });
 
   if (updateError)
     throw new Error(`UpdateStorageError: ${updateError.message}`);
