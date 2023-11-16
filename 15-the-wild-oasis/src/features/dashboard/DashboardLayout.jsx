a2dc243c9438c849437271902c9e7c31b9599a9e
martinsndifon <martinsndifon@gmail.com>
2023-11-16 19:02:08 +0100

    Building the sales chart

--- 15-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 15-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import { useRecentBookings } from './useRecentBookings';
 import { useRecentStays } from './useRecentStays';
 import Stats from './Stats';
 import { useCabins } from '../cabins/useCabins';
+import SalesChart from './SalesChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -34,7 +35,7 @@ function DashboardLayout() {
       />
       <div>Today activity</div>
       <div>Chart stay duration</div>
-      <div>Chart sales</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
