8a38a9223201e537a0443eea439e484ffb29378e
martinsndifon <martinsndifon@gmail.com>
2023-11-16 10:03:59 +0100

    Implementing dark mode in the app

---
+++ 15-the-wild-oasis/src/context/DarkModeContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useEffect } from 'react';
+import { useLocalStorageState } from '../hooks/useLocalStorageState';
+
+export const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, 'isDarkMode');
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark-mode');
+      document.documentElement.classList.remove('light-mode');
+    } else {
+      document.documentElement.classList.add('light-mode');
+      document.documentElement.classList.remove('dark-mode');
+    }
+  }, [isDarkMode]);
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark);
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+export { DarkModeProvider };
---
+++ 15-the-wild-oasis/src/context/customContextHooks.js
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import { DarkModeContext } from './DarkModeContext';
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+  if (context === undefined)
+    throw new Error('DarkModeContext was used outside of the DarkModeProvider');
+
+  return context;
+}
+
+export { useDarkMode };
