cc4058d87461d065586150c74403da9894899f9f
martinsndifon <martinsndifon@gmail.com>
2023-11-17 13:14:39 +0100

    optimized for deployment

--- 15-the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ 15-the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import useLogin from './useLogin';
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState('martins@example.com');
-  const [password, setPassword] = useState('password');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const { isLoading, login } = useLogin();
 
